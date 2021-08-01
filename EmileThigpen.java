38e754630486f75b548047b947d7c7910743bd3f
Emile Thigpen <emilethigpen@gmail.com>
2021-08-01 16:26:37 -0500

    my first java programs!

---
+++ EmileThigpen.java
@@ -0,0 +1,7 @@
+public class EmileThigpen{
+    public static void main(String[] args){
+        System.out.println("My name is Emile´ Thigpen");
+        System.out.println("I am 30 years old");
+        System.out.println("I live in Birmingham, AL");
+    }
+}
